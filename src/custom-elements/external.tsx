1c9219f2f52f8a885ec823d03dfa8aff679ace7e
Andreas Pfeil <andreas.pfeil@kit.edu>
2023-09-21 22:03:41 +0200

    fix(build): comment currently unused callback

--- src/custom-elements/external.tsx
+++ src/custom-elements/external.tsx
@@ -4,15 +4,15 @@ import { CustomElement } from './index';
 export default class ExternalEl implements CustomElement {
   public static id = '@id';
   public value: string = "";
-  private callback?: (value: any) => void;
+  //private callback?: (value: any) => void;
 
-  constructor(value: any, callback?: (value: any) => void) {
+  constructor(value: any, /*callback?: (value: any) => void*/) {
     if (typeof value['@id'] === 'string') {
       this.value = value['@id'];
     } else {
       console.error('Value is not of type string', value)
     }
-    this.callback = callback;
+    //this.callback = callback;
   }
 
   render() {
