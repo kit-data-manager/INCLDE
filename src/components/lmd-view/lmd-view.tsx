e9c4a252744e47f5faac9fd9ad46018ef1b1cb5f
Andreas Pfeil <andreas.pfeil@kit.edu>
2023-09-21 15:48:10 +0200

    fix: avoid stencil caching warnings

--- src/components/lmd-view/lmd-view.tsx
+++ src/components/lmd-view/lmd-view.tsx
@@ -24,11 +24,6 @@ export class LmdView {
 
   private config: ConfigHelper = ConfigHelper.getInstance();
 
-  // svg graphic elements
-  private svgDelete: SVGElement = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>;
-  private svgPutElementUp: SVGElement = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" /></svg>;
-  private svgPutElementDown: SVGElement = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" /></svg>;
-
   private deleteElement(selector: (string|number)[]) {
     pathTraversalSet(this.data, selector, undefined);
     this.redraw.emit();
@@ -108,7 +103,7 @@ export class LmdView {
   private renderDeleteButton(key: string) {
     return <div class="interface-buttons offset">
       <button class="danger-btn" onClick={() => this.deleteElement([this.selectedIndex!, key])}>
-        {this.svgDelete}
+        {this.renderDeleteIcon()}
       </button>
     </div>;
   }
@@ -123,11 +118,23 @@ export class LmdView {
    */
   private renderThreeInterfaceButtons(index: number, key: string, value: any) {
     return <div class="interface-buttons">
-      {index !== 0 && <button onClick={() => this.swapElements([this.selectedIndex!, key], index - 1, index)}>{this.svgPutElementUp}</button>}
-      {index !== (value.length - 1) && <button onClick={() => this.swapElements([this.selectedIndex!, key], index, index + 1)}>{this.svgPutElementDown}</button>}
+      {index !== 0 && <button onClick={() => this.swapElements([this.selectedIndex!, key], index - 1, index)}>{this.renderPutUpIcon()}</button>}
+      {index !== (value.length - 1) && <button onClick={() => this.swapElements([this.selectedIndex!, key], index, index + 1)}>{this.renderPutDownIcon()}</button>}
       {index === (value.length - 1) && <button disabled />}
-      <button class="danger-btn" onClick={() => this.deleteElement([this.selectedIndex!, key, index])}>{this.svgDelete}</button>
+      <button class="danger-btn" onClick={() => this.deleteElement([this.selectedIndex!, key, index])}>{this.renderDeleteIcon()}</button>
     </div>;
+  }
+
+  private renderPutDownIcon(): SVGElement {
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" /></svg>;
+  }
+
+  private renderPutUpIcon(): SVGElement {
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M182.6 137.4c-12.5-12.5-32.8-12.5-45.3 0l-128 128c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8H288c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-128-128z" /></svg>;
+  }
+
+  private renderDeleteIcon() {
+    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" /></svg>;
   }
 
   render() {
